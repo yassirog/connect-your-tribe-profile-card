fec04dabd5300f4b52f11bd514ffecbb3d808173
yassir bghiel <yassir.bghiel@hva.nl>
2024-02-09 00:14:11 +0100

    push

--- public/script/script.js
+++ public/script/script.js
@@ -1,14 +1,17 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+  // Navbar selectie functie
+const aboutTab = document.getElementById('about-tab');
+const fullinfoTab = document.getElementById('fullinfo-tab');
+const aboutSection = document.querySelector('.about');
+const fullinfoSection = document.getElementById('fullinfo-card');
 
-function expand () {
-  document.body.classList.toggle('expand')
-}
+  // Add click event listener to 'Full Info' tab
+  fullinfoTab.addEventListener('click', function () {
+  aboutSection.style.display = 'none';
+  fullinfoSection.style.display = 'block';
+  });
 
-function changeImage() {
-  if (document.getElementById("imgClickAndChange").src == "https://media.discordapp.net/attachments/620743305713680385/1149644670532591687/vacation_3.png?width=1034&height=1034"){
-      document.getElementById("imgClickAndChange").src = "https://media.discordapp.net/attachments/620743305713680385/1149641421188497439/outfitalleen.png?width=1034&height=1034";
-  } else {
-      document.getElementById("imgClickAndChange").src = "https://media.discordapp.net/attachments/620743305713680385/1149644670532591687/vacation_3.png?width=1034&height=1034";
-  }
-}
+  // Add click event listener to 'About' tab
+  aboutTab.addEventListener('click', function () {
+    aboutSection.style.display = 'block';
+    fullinfoSection.style.display = 'none';
+  });
